39996c0b7b5f8686193e504aaaa6df4b51aa2d56
Taiara <taiarachase@gmail.com>
2022-10-17 17:28:19 -0300

    first commit

---
+++ Media.py
@@ -0,0 +1,33 @@
+# Taiara Regis - 16/10/2022
+# Programa que lê um número X de listas e mostra quantas listas tinham somente números Y
+# e quantas listas tinham somente números Z
+
+from random import randint
+
+cont = 0
+cont0 = 0
+cont1 = 0
+
+squares = ([])
+
+while True:
+    for square in range(1, 11):
+        squares.append(randint(0, 1))
+    if sum(squares) == 0:
+        cont0 +=1
+    elif sum(squares) == 10:
+        cont1 += 1
+    else:
+        cont += 1
+    if cont0 == 10 or cont1 == 10:
+        break
+    squares = ([])
+
+print('-'*100)
+print(f'Foram analisadas {cont} listas, cada uma contendo 10 valores de 0 a 1')
+print(f'{cont0} listas continham somente o número 0')
+print(f'{cont1} listas continham somente o número 1')
+print('-'*100)
+print(f'{cont0/cont*100:.2f}% das listas são compostas somente de 0')
+print(f'{cont1/cont*100:.2f}% das listas são compostas somente de 1')
+print('-'*100)
---
+++ README.txt
@@ -0,0 +1,4 @@
+Programa que lê um número X de listas, compostas somente de números Y e Z
+em seguida contabiliza quantas continham somente números Y ou Z
+ao contabilizar um total de 10 listas contendo apenas Y ou Z, o programa se encerra e mostra os resultados finais
+quantas listas foram contabilizadas, o número de listas contendo apenas Y ou Z
